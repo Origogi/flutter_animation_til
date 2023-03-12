b49d66c132386e38b9188b47bae3664391af98f3
tony.origogi <tony.origogi@kakaobrain.com>
2023-03-12 11:26:40 +0900

    implement clock hand

--- stopwatch_flutter/lib/ui/stopwatch.dart
+++ stopwatch_flutter/lib/ui/stopwatch.dart
@@ -1,41 +1,51 @@
-import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:stopwatch_flutter/ui/elapsed_time_text.dart';
-import 'package:stopwatch_flutter/ui/elapsed_time_text_basic.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:stopwatch_flutter/ui/stopwatch_renderer.dart';
 
 class Stopwatch extends StatefulWidget {
   @override
   _StopwatchState createState() => _StopwatchState();
 }
 
-class _StopwatchState extends State<Stopwatch> {
-
-  late DateTime _initialTime;
+class _StopwatchState extends State<Stopwatch>
+    with SingleTickerProviderStateMixin {
   Duration _elapsed = Duration.zero;
-  late final Timer _timer;
+  late final Ticker _ticker;
+
   @override
   void initState() {
     super.initState();
-    _initialTime = DateTime.now();
-    _timer = Timer.periodic(Duration(milliseconds: 50), (timer) {
-      final now = DateTime.now();
+
+    _ticker = createTicker((elapsed) {
       setState(() {
-        _elapsed = now.difference(_initialTime);
+        _elapsed = elapsed;
       });
     });
+    _ticker.start();
   }
 
   @override
   void dispose() {
-    _timer.cancel();
+    _ticker.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ElapsedTimeText(
-      elapsed: _elapsed,
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      final radius = min(constraints.maxWidth, constraints.maxHeight) / 2;
+      return Stack(
+        children: [
+          Center(
+            child: StopwatchRenderer(
+              radius: radius,
+              elapsed: _elapsed,
+            ),
+          ),
+        ],
+      );
+    });
   }
 }
