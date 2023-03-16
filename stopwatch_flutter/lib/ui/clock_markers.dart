90b6d97818fa04cbd72f45e6a4b3138075937975
tony.origogi <tony.origogi@kakaobrain.com>
2023-03-16 20:44:08 +0900

    update

--- stopwatch_flutter/lib/ui/clock_markers.dart
+++ stopwatch_flutter/lib/ui/clock_markers.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ClockSecondsTickMarker extends StatelessWidget {
   const ClockSecondsTickMarker(
@@ -28,5 +27,41 @@ class ClockSecondsTickMarker extends StatelessWidget {
         color: color,
       ),
     );
+  }
+}
+
+class ClockTextMaker extends StatelessWidget {
+  const ClockTextMaker(
+      {Key? key,
+      required this.value,
+      required this.maxValue,
+      required this.radius})
+      : super(key: key);
+
+  final int value;
+  final int maxValue;
+  final double radius;
+
+  @override
+  Widget build(BuildContext context) {
+    const width = 40.0;
+    const height = 30.0;
+
+    return Transform(
+        alignment: Alignment.center,
+        transform: Matrix4.identity()
+          ..translate(-width / 2, -height / 2, 0.0)
+          ..rotateZ(pi + 2 * pi * (value / maxValue))
+          ..translate(0.0, radius - 35, 0.0)
+          ..rotateZ(pi - 2 * pi * (value / maxValue)),
+        child: SizedBox(
+          width: width,
+          height: height,
+          child: Text(
+            value.toString(),
+            style: TextStyle(color: Colors.white, fontSize: 24),
+            textAlign: TextAlign.center,
+          ),
+        ));
   }
 }
--- stopwatch_flutter/lib/ui/stopwatch_renderer.dart
+++ stopwatch_flutter/lib/ui/stopwatch_renderer.dart
@@ -23,6 +23,12 @@ class StopwatchRenderer extends StatelessWidget {
               left: radius,
               top: radius,
               child: ClockSecondsTickMarker(seconds: i, radius: radius)),
+
+        for (var i =5;i<=60;i +=5)
+          Positioned(
+              left: radius,
+              top: radius,
+              child: ClockTextMaker(value: i, maxValue: 60, radius: radius)),
         Positioned(
           left: radius,
           top: radius,
