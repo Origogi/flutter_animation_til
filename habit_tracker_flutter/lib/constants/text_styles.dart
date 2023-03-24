fc84ec938e25a1a8ca8a2867efde764a516c1d46
tony.origogi <tony.origogi@kakaobrain.com>
2023-03-24 17:46:40 +0900

    Update

--- habit_tracker_flutter/lib/constants/text_styles.dart
+++ habit_tracker_flutter/lib/constants/text_styles.dart
@@ -2,26 +2,26 @@ import 'package:flutter/material.dart';
 
 /// All the text styles used in the app
 class TextStyles {
-  // Used when showing the task name in the main grid
+  // Used when showing the task.dart name in the main grid
   static const taskName = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 17,
     letterSpacing: 0.0,
   );
 
-  // Large text (for add/edit task pages)
+  // Large text (for add/edit task.dart pages)
   static const heading = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 24,
   );
 
-  // Regular text (for add/edit task pages)
+  // Regular text (for add/edit task.dart pages)
   static const content = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 22,
   );
 
-  // Small text (for add/edit task pages)
+  // Small text (for add/edit task.dart pages)
   static const caption = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 16,
